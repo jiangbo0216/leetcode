fe7d5cb816fdfedc10dc8eb038811bf66065978e
jiangbo <jiangbo0216@gmail.com>
2020-05-06 00:35:12 +0800

    feat(leetcode 1-5): 1-5

---
+++ 1-array-两数之和/两数之和.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=javascript
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  var ret = [];
+  var exist = {};
+  for(var i = 0; i < nums.length; i++){
+      if(typeof(exist[target - nums[i]]) !== 'undefined'){ // 使用typeof比较准确
+          ret.push(exist[target - nums[i]]);
+          ret.push(i + 1);
+      }
+      
+      exist[nums[i]] = i + 1;
+  }
+  
+  return ret
+};
+
+// @lc code=end
+
